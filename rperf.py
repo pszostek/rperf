6a228b7c161da816268ba3415add0f98a809e611
Pawel Szostek <pawel.szostek@cern.ch>
2014-12-12 14:31:55 +0100

    keep results in the same order as runs appear in the conf

--- rperf.py
+++ rperf.py
@@ -9,7 +9,7 @@ try:
     from collections import OrderedDict
 except ImportError:
     from OrderedDict import OrderedDict
-from threading import Thread
+from threading import Thread, Event
 import os
 from termcolor import cprint, colored
 from time import localtime, strftime
@@ -17,38 +17,63 @@ from prettytable import from_csv
 from Queue import Queue
 from collections import deque, defaultdict
 from StringIO import StringIO
+from copy import copy, deepcopy
 import json
 
 
-def get_conf(conf_file):
-    def parse_single(single_run):
-        if 'id' not in single_run:
-            single_run['id'] = single_run['binary']
-        if 'host' not in single_run:
-            single_run['host'] = 'localhost'
-        if 'env' in single_run:
-            assert isinstance(single_run['env'], dict)
-        if 'user' not in single_run:
-            single_run['user'] = None
-        if 'precmd' not in single_run:
-            single_run['precmd'] = None
-        assert 'events' in single_run or 'pfm-events' in single_run
-        if 'events' not in single_run:
-            single_run['events'] = None
-        if 'pfm-events' not in single_run:
-            single_run['pfm-events'] = None
+def make_hash(dict_):
+    """
+    http://stackoverflow.com/questions/5884066/hashing-a-python-dictionary
 
-    with open(conf_file, 'r') as conf:
-        lines = conf.readlines()
+    Makes a hash from a dictionary, list, tuple or set to any level, that contains
+    only other hashable types (including any lists, tuples, sets, and
+    dictionaries).
+    """
+    if isinstance(dict_, (set, tuple, list)):
+        return tuple([make_hash(e) for e in dict_])    
+    elif not isinstance(dict_, dict):
+        return hash(dict_)
+
+    new_dict = deepcopy(dict_)
+    for k, v in new_dict.items():
+        new_dict[k] = make_hash(v)
+
+    return hash(tuple(frozenset(sorted(new_dict.items()))))
+
+def get_conf(conf_filename):
+    def sanitize_single(run):
+        run_cpy = copy(run)
+        if 'id' not in run_cpy:
+            # run_cpy['id'] = run_cpy['binary']
+            run_cpy['id'] = None
+        if 'host' not in run_cpy:
+            run_cpy['host'] = 'localhost'
+        if 'env' in run_cpy:
+            assert isinstance(run_cpy['env'], dict)
+        if 'user' not in run_cpy:
+            run_cpy['user'] = None
+        if 'precmd' not in run_cpy:
+            run_cpy['precmd'] = None
+        assert 'events' in run_cpy or 'pfm-events' in run_cpy
+        if 'events' not in run_cpy:
+            run_cpy['events'] = None
+        if 'pfm-events' not in run_cpy:
+            run_cpy['pfm-events'] = None
+        return run_cpy
+
+    with open(conf_filename, 'r') as conf_file:
+        lines = conf_file.readlines()
         content = ''.join(lines)
         conf_json = json.loads(content)
+
+    sanitized = []
     if isinstance(conf_json, list):
         for item in conf_json:
-            parse_single(item)
-        return conf_json
+            sanitized.append(sanitize_single(item))
+        return sanitized
     else:
-        parse_single(conf_json)
-        return list(conf_json)
+        sanitized = sanitize_single(conf_json)
+        return list(sanitized)
 
 
 def cut_out_comments(hostfile_lines):
@@ -93,34 +118,42 @@ def parse_perf(perf_output, include_time):
                     pass
     return stats
 
+
 def average_results(results):
     # results is a dictionary: {id : [res1, res2, res3]}
     # turn it into {id : res}
     def average(list_):
-        return sum(list_)/float(len(list_))
-    ret = {}
+        return sum(list_) / float(len(list_))
+    ret = OrderedDict()
     for id_, res_list in results.items():
-        average_res = {}
+        average_res = OrderedDict()
         metrics = res_list[0].keys()
         for metric in metrics:
             try:
-                average_res[metric] = average([result[metric] for result in res_list])
-            except:
+                average_res[metric] = average(
+                    [float(result[metric]) for result in res_list])
+            except ValueError:
                 average_res[metric] = res_list[0][metric]
         ret[id_] = average_res
     return ret
 
-def gather_results(
-        conf, verbose, include_time, grab_output, print_stdout, print_stderr, times):
-    results = OrderedDict()
-    stdout_queue = Queue()
-    done_hosts_queue = Queue()
-    jobs_number = len(conf)*times
+class PrinterThread(Thread):
+    def __init__(self, stdout_queue, total_jobs):
+        super(PrinterThread, self).__init__()
+        self._stop = Event()
+        self.stdout_queue = stdout_queue
+        self.total_jobs = total_jobs
 
-    def printer(stdout_queue, total_jobs):
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def run(self):
         summary_count = 0
-        while True:
-            message = stdout_queue.get()
+        while not self._stop.isSet():
+            message = self.stdout_queue.get()
             if message is None:  # job is done
                 break
             else:
@@ -130,80 +163,99 @@ def gather_results(
                     (colored(
                         "[{job}/{total_jobs}]".format(
                             job=summary_count,
-                            total_jobs=total_jobs),
+                            total_jobs=self.total_jobs),
                         "magenta",
                         attrs=["bold"]),
-                        message), end="")
-            stdout_queue.task_done()
+                        message))
+            self.stdout_queue.task_done()
 
-    def gather_single(command, id_, host, user, results,
-                      stdout_queue, done_hosts_queue, grab_output=False):
-        if host != "localhost":
-            if user:
-                ssh_serv = "%s@%s" % (user, host)
-            else:
-                ssh_serv = host
-            command = "ssh {user_server} '{command}'".format(
-                user_server=ssh_serv,
-                command=command)
-
-        if verbose:
-            print("Running command %s" % command)
-
-        command_pipe = Popen(command,
-                             shell=True,
-                             stdout=PIPE,
-                             stdin=PIPE,
-                             stderr=PIPE)
-
-        stdout, stderr = command_pipe.communicate()
-        time_ = strftime("%H:%M:%S", localtime())
-
-        output = ""
-        if command_pipe.returncode != 0:
-            output += "{time} {failure} {host}\n".format(
-                time=time_,
-                failure=colored("[FAILED]", "red"),
-                host=host)
+def gather_single(command, id_, host, user, results,
+                  stdout_queue, done_hosts_queue, include_time, verbose, print_stdout, print_stderr, grab_output=False):
+    if host != "localhost":
+        if user:
+            ssh_serv = "%s@%s" % (user, host)
         else:
-            output += "{time} {success} {host}\n".format(
-                time=time_,
-                success=colored("[SUCCESS]", "green"),
-                host=host
-            )
-            result = parse_perf(stderr, include_time=include_time)
+            ssh_serv = host
+        command = "ssh {user_server} '{command}'".format(
+            user_server=ssh_serv,
+            command=command)
 
-            if grab_output:
-                result["output"] = stdout.strip()
+    if verbose:
+        print("Running command: %s" % colored(command, attrs=["bold"]))
 
-            if id_ in results:
-                results[id_].append(result)
-            else:
-                results[id_] = [result]
-        done_hosts_queue.put(host)
+    command_pipe = Popen(command,
+                         shell=True,
+                         stdout=PIPE,
+                         stdin=PIPE,
+                         stderr=PIPE)
 
-        if verbose or print_stdout:
-            if not stdout:
-                stdout = ""
-            output += "{stdout_name}\n{stdout}".format(stdout_name=colored("stdout", "green", attrs=['bold']),
-                                                       stdout=stdout)
+    stdout, stderr = command_pipe.communicate()
+    time_ = strftime("%H:%M:%S", localtime())
 
-        if verbose or print_stderr:
-            if not stderr:
-                stderr = ""
-            output += "{stderr_name}\n{stderr}".format(stderr_name=colored("stderr", "red", attrs=['bold']),
-                                                       stderr=stderr)
-        stdout_queue.put(output)
+    output = ""
+    if command_pipe.returncode != 0:
+        output += "{time} {failure} {host}\n".format(
+            time=time_,
+            failure=colored("[FAILED]", "red"),
+            host=host)
+    else:
+        output += "{time} {success} {host}\n".format(
+            time=time_,
+            success=colored("[SUCCESS]", "green"),
+            host=host
+        )
+        result = parse_perf(stderr, include_time=include_time)
+
+        if grab_output:
+            result["output"] = stdout.strip()
+
+        if id_ in results:
+            results[id_].append(result)
+        else:
+            results[id_] = [result]
+    done_hosts_queue.put(host)
+
+    if verbose or print_stdout:
+        if not stdout:
+            stdout = ""
+        output += "{stdout_name}\n{stdout}".format(stdout_name=colored("stdout", "green", attrs=['bold']),
+                                                   stdout=stdout)
+
+    if verbose or print_stderr:
+        if not stderr:
+            stderr = ""
+        output += "{stderr_name}\n{stderr}".format(stderr_name=colored("stderr", "red", attrs=['bold']),
+                                                   stderr=stderr)
+    stdout_queue.put(output)
+
+def gather_results(
+        conf, verbose, include_time, grab_output, print_stdout, print_stderr, times, randomize):
+
+
+    results = OrderedDict()
+    stdout_queue = Queue()
+    done_hosts_queue = Queue()
+    jobs_number = len(conf) * times
 
     # queue of watinig jobs. keys are hostnames, values are waiting threads
     jobs = defaultdict(deque)
     jobs_total = 0
     jobs_done = 0
     runs = []
-    for conf_run_id, conf_run in enumerate(conf):
+    hash_run_dict = OrderedDict()
+    for conf_run in conf:
+        hash_ = make_hash(conf_run) 
+        print(conf_run, hash_)
+        hash_run_dict[hash_] = conf_run
         for _ in xrange(times):
-            runs.append((conf_run_id, conf_run))
-    for run_id, run in runs: # create all the threads, but don't start them yet
+            runs.append(conf_run)
+
+    if randomize:
+        import random
+        random.shuffle(runs)
+
+    # create all the threads, but don't start them yet
+    for run in runs:
         command = make_remote_command(events=run["events"],
                                       pfm_events=run["pfm-events"],
                                       precmd=run["precmd"],
@@ -212,39 +264,62 @@ def gather_results(
         host = run["host"]
         user = run["user"]
         id_ = run["id"]
+        hash_ = make_hash(run) # when saving the results, the key will be a hash instead of user-given id
         job = Thread(
             target=gather_single,
             args=(
                 command,
-                id_,
+                hash_,
                 host,
                 user,
                 results,
                 stdout_queue,
                 done_hosts_queue,
+                include_time,
+                verbose,
+                print_stdout,
+                print_stderr,
                 grab_output))
         jobs[run["host"]].append(job)
         jobs_total += 1
 
-    printer_thread = Thread(target=printer, args=(stdout_queue, jobs_number))
+    printer_thread = PrinterThread(stdout_queue, jobs_number)
     printer_thread.start()
 
-    # bootstrap one job per host
-    for thread in [hostjobs[0] for hostjobs in jobs.values()]:
-        thread.start()
+    try:
+        # bootstrap one job per host
+        for thread in [hostjobs[0] for hostjobs in jobs.values()]:
+            thread.start()
 
-    while jobs_total != jobs_done:
-        host_done = done_hosts_queue.get()
-        jobs[host_done][0].join()
-        jobs_done += 1
-        jobs[host_done].popleft()  # forget about terminated thread
-        if len(jobs[host_done]) != 0:
-            jobs[host_done][0].start()
+        while jobs_total != jobs_done:
+            host_done = done_hosts_queue.get()
+            jobs[host_done][0].join()
+            jobs_done += 1
+            jobs[host_done].popleft()  # forget about terminated thread
+            if len(jobs[host_done]) != 0:
+                jobs[host_done][0].start()
+    except KeyboardInterrupt:
+        for host, host_jobs in jobs.items():
+            for host_job in host_jobs:
+                try:
+                    host_job.join()
+                except RuntimeError: # trying to join unstarted Thread
+                    pass
+        printer_thread.stop()
+        printer_thread.join()
+        print(colored("Execution interrupted. %d jobs were finished." % jobs_done, "red", attrs=["bold"]))
+        sys.exit(1)
 
     stdout_queue.put(None)
     printer_thread.join()
 
-    return average_results(results)
+    unhashed_results = OrderedDict()
+    averaged = average_results(results)
+    averaged = averaged.items()
+    averaged = sorted(averaged, key=lambda x: hash_run_dict.keys().index(x[0]))
+    for hash_, result in averaged:
+        unhashed_results[hash_run_dict[hash_]["id"]] = result
+    return unhashed_results
 
 
 def flip_dictionary(ddict):
@@ -269,9 +344,10 @@ def output_results(results, output_buffer, hosts_vertically):
         event_names = results[first_host].keys()
         events_str = ',' + ','.join(event_names) + '\n'
         output_buffer.write(events_str)
-        for hostname, values_on_host in results.iteritems():
-            row = hostname + ',' + \
-                ','.join([str(value) for value in values_on_host.values()])
+        for id_, values_on_host in results.iteritems():
+            if id_ is None:
+                id_ = ""
+            row = ','.join((id_,','.join([str(value) for value in values_on_host.values()])))
             output_buffer.write(row + '\n')
 
     else:  # hosts horizontally, events vertically
@@ -286,7 +362,7 @@ def output_results(results, output_buffer, hosts_vertically):
 
 def make_remote_command(events, pfm_events, precmd, env, command):
     binary = command
-    command = "perf stat "
+    command = "perf stat"
 
     if pfm_events:
         command += " --pfm-events %s " % ','.join(pfm_events)
@@ -334,6 +410,12 @@ if __name__ == "__main__":
         metavar="N",
         help="Repeat each measurement N times and take an average of the results",
         default=1)
+    parser.add_option(
+        '-r',
+        '--randomize',
+        dest="randomize",
+        help="Randomize order of runs",
+        action="store_true")
     parser.add_option(
         '--dump',
         dest="dump",
@@ -414,7 +496,7 @@ if __name__ == "__main__":
     else:
         output_path = "rperf.csv"
 
-    conf = get_conf(conf_file=options.conf)
+    conf = get_conf(conf_filename=options.conf)
 
     perf_stats = gather_results(conf=conf,
                                 verbose=verbose,
@@ -422,8 +504,10 @@ if __name__ == "__main__":
                                 grab_output=options.grab_output,
                                 print_stdout=options.inline_stdout,
                                 print_stderr=options.inline_stderr,
-                                times=int(options.times))
+                                times=int(options.times),
+                                randomize=options.randomize)
 
+    print(perf_stats)
     output_buffer = StringIO()
     output_results(results=perf_stats,
                    output_buffer=output_buffer,
